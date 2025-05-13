993e124c334af6260b290fa28f413d6b080a7474
bhagi123659 <sanapurvarnitha@gmail.com>
2025-05-13 17:45:55 +0530

    Create Marks of student.c

---
+++ Marks of student.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter student's marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 40) {
+        printf("The student passed.\n");
+    } else {
+        printf("The student failed.\n");
+    }
+
+    return 0;
+}
