63113adc562669eb6db18e3a912ab7c310a68eef
bhagi123659 <sanapurvarnitha@gmail.com>
2025-05-13 15:42:36 +0530

    Create siet.c

---
+++ siet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int siet()
+ {
+     printf("bhagirati\n");
+     printf("9-4-2006\n");
+     printf("18\n");
+     printf("8217533491\n");
+ }
+ int main()
+ {
+     siet();
+     
+     return 0;
+ }
