dd8123894cf971172807e88f013ad2e7c891d046
bhagi123659 <sanapurvarnitha@gmail.com>
2025-05-14 09:40:31 +0530

    Update Smallest 3 no.c

--- Smallest 3 no.c
+++ Smallest 3 no.c
@@ -3,11 +3,11 @@
 int main() {
     int a, b, c;
 
-    // Input three numbers
+    
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    // Check which is the smallest
+    
     if (a <= b && a <= c) {
         printf("%d is the smallest number.\n", a);
     } else if (b <= a && b <= c) {
