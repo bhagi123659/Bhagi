9af765525a57b3b818f1895349a4ed57dc5059d2
bhagi123659 <sanapurvarnitha@gmail.com>
2025-05-15 15:34:00 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+    int num,res=0,rem,org;
+    printf("enter the number");
+    scanf("%d",&num);
+    org=num;
+    while(num!=0){
+        rem=num%10;
+        res+=rem*rem*rem;
+        num=num/10;
+    }
+    if(org==res){
+        printf("the number is armstrong");
+    }
+    else{
+        printf("the number is not armstrong");
+    }
+}
